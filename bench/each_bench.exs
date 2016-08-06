5b9ecb65de657e7a6e18dea2273a8314c2288303
Andrew Dryga <andrew@dryga.com>
2016-08-06 14:15:57 +0300

    More bench samples to for better display of linear efficiency correlation

--- bench/each_bench.exs
+++ bench/each_bench.exs
@@ -6,7 +6,8 @@ defmodule EachBench do
       single: "test/fixtures/1.bson",
       small: "test/fixtures/30.bson",
       medium: "test/fixtures/300.bson",
-      large: "test/fixtures/30000.bson"
+      large: "test/fixtures/3000.bson",
+      xlarge: "test/fixtures/30000.bson"
     ]
   end
 
@@ -31,8 +32,15 @@ defmodule EachBench do
     |> File.close
   end
 
-  bench "read and map 30_000 documents", [fixtures: get_fixtures()] do
+  bench "read and map 3_000 documents", [fixtures: get_fixtures()] do
     fixtures[:large]
+    |> File.open!([:read, :binary, :raw])
+    |> BSONEach.each(&foo/1)
+    |> File.close
+  end
+
+  bench "read and map 30_000 documents", [fixtures: get_fixtures()] do
+    fixtures[:xlarge]
     |> File.open!([:read, :binary, :raw])
     |> BSONEach.each(&foo/1)
     |> File.close
